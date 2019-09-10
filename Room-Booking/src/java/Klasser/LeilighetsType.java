ca29cdd709ff6e8fb7f38c8c1f936eaeba524fa4
Are <arefjerm@gmail.com>
2019-09-10 13:15:26 +0200

    ny leilighetstype v0.1

---
+++ Room-Booking/src/java/Klasser/LeilighetsType.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Klasser;
+
+import java.io.PrintWriter;
+import java.sql.*;
+import javax.naming.*;
+import javax.sql.DataSource;
+import Klasser.DbTool;
+
+/**
+ *
+ * @author Are
+ */
+public class LeilighetsType {
+    Connection conn;
+    Statement stmt;
+    
+    public void Insert(PrintWriter out, String Navn, String Kategori, String Enkeltsenger, String Dobeltsenger, String Beskrivelse, String Pris){
+        out.println("Are er awsome!");
+        DbTool dbTool = new DbTool();
+        conn = dbTool.loggInn(out);
+        try{
+            DatabaseMetaData dbm = conn.getMetaData();
+        
+            ResultSet table = dbm.getTables(null, null, "LeilighetsType", null);
+            if(table.next()){
+                CreateTable(out, conn);
+                out.println("Are er awsome!!!!!");
+            } else {out.println("Are er awsome!!!");}
+            
+            
+            String sql = "INSERT INTO LeilighetsType (Navn, Kategori, Enkeltsenger, Dobeltsenger, Beskrivelse, Pris) "
+                    + "VALUES (?, ?, ?, ?, ?, ?)";
+            
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, Navn);
+            statement.setString(2, Kategori);
+            statement.setString(3, Enkeltsenger);
+            statement.setString(4, Dobeltsenger);
+            statement.setString(5, Beskrivelse);
+            statement.setString(6, Pris);
+ 
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                out.println("A new user was inserted successfully!");
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            out.println("Are er ikke awsome");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    private void CreateTable(PrintWriter out, Connection conn){
+        try {
+        
+        //stmt = conn.createStatement();
+        String sql = "create table LeilighetsType(ID int AUTO_INCREMENT=10000, Navn varchar(30), Kategori varchar(20),"
+                + " Enkeltsenger int,Dobeltsenger int, Beskrivelse varchar(150), Pris int, PublisertStatus bool,"
+                + "constraint PK_LeilighetsType primary key (ID));";
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.executeUpdate();
+        out.println("Are er awsome!!!!!!!!!!");
+        } catch (SQLException e){
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Room-Booking/src/java/Servlets/CreateLeilighetstype.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import Klasser.LeilighetsType;
+
+/**
+ *
+ * @author Are
+ */
+@WebServlet(name = "CreateRomtype", urlPatterns = {"/createleilighetstype"})
+public class CreateLeilighetstype extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("Are er awsome");
+            String Navn = request.getParameter("Navn");
+            String Enkeltsenger = request.getParameter("Enkeltsenger");
+            String Dobeltsenger = request.getParameter("Dobeltsenger");
+            String Beskrivelse = request.getParameter("Beskrivelse");
+            String Pris = request.getParameter("Pris");
+            String Kategori = request.getParameter("Kategori");
+            String Egenskaper = request.getParameter("Egenskaper");
+            
+            LeilighetsType leilighetsType = new LeilighetsType();
+            leilighetsType.Insert(out, Navn, Kategori, Enkeltsenger, Dobeltsenger, Beskrivelse, Pris);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
