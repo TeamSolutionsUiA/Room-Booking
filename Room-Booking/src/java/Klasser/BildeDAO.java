c0629fea4e63c8e17991eaa2250d586ff4a667ad
Jonathan <jonatl14@uia.no>
2019-09-17 12:40:01 +0200

    Metode readAll på klassen BildeDAO. For å hente en liste med alle bilder fra et rom.

--- Room-Booking/src/java/Klasser/BildeDAO.java
+++ Room-Booking/src/java/Klasser/BildeDAO.java
@@ -8,13 +8,22 @@ package Klasser;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Files;
+
 
 /**
  *
  * @author arefj
  */
 public class BildeDAO {
+    
+       private List<Bilde> liste;
+        
     public void Insert(Connection conn, Bilde bilde, int id){
         try {
                 InputStream nyttBilde = bilde.getBilde();
@@ -30,4 +39,26 @@ public class BildeDAO {
             e.printStackTrace();
         }
     }
-}
+    
+    public List<Bilde> readAll(Connection conn, String LelighetsID){
+        
+     
+        try {
+            String query = "SELECT * FROM Bilde where Leilighet_ID=?";
+            PreparedStatement stm =conn.prepareStatement(query);
+            stm.setString(1,LelighetsID);
+            ResultSet rs = stm.executeQuery();
+       
+            while(rs.next()){
+                liste.add(new Bilde(rs.getBinaryStream("Bilde")));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return liste;
+        }
+    }
+    
+
