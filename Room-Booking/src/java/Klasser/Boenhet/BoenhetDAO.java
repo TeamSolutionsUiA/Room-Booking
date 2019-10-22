cf39be8101b67bc51884776c1db3c6214745a731
Rexsect <46561873+Rexsect@users.noreply.github.com>
2019-10-22 09:13:19 +0200

    Fix på delete

--- Room-Booking/src/java/Klasser/Boenhet/BoenhetDAO.java
+++ Room-Booking/src/java/Klasser/Boenhet/BoenhetDAO.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Klasser;
+package Klasser.Boenhet;
 
+import Klasser.DbTool;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
